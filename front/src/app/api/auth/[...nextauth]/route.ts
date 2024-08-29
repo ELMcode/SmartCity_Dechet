8216a280b90d28491b2a3a15225b68e1f25ccd13
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-08-29 15:18:02 +0200

    fix register

--- front/src/app/api/auth/[...nextauth]/route.ts
+++ front/src/app/api/auth/[...nextauth]/route.ts
@@ -2,34 +2,6 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { User } from '@/lib/definition';
 
-const mockDatabase: (User & { password: string })[] = [
-    {
-        id: '1',
-        email: 'test@test.com',
-        password: 'password123',
-        firstName: 'John',
-        lastName: 'Doe',
-        address: {
-            number: '123',
-            street: 'Main St',
-            complement: '',
-            postalCode: '12345'
-        }
-    },
-    {
-        id: '2',
-        email: 'jane@doe.com',
-        password: 'mypassword',
-        firstName: 'Jane',
-        lastName: 'Doe',
-        address: {
-            number: '456',
-            street: 'Second St',
-            complement: 'Apt 1',
-            postalCode: '67890'
-        }
-    }
-];
 
 const handler = NextAuth({
 
@@ -51,7 +23,7 @@ const handler = NextAuth({
 
                 const { email, password } = credentials;
 
-                const user = mockDatabase.find(user => user.email === email && user.password === password);
+                // const user = mockDatabase.find(user => user.email === email && user.password === password);
 
                 if (user) {
                     const { password, ...userWithoutPassword } = user;
