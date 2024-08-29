5ffdfd9cc9acb2eb0a149ed35dad9c3b98b6519e
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-08-29 12:13:58 +0200

    auth

--- front/src/app/api/auth/[...nextauth]/route.ts
+++ front/src/app/api/auth/[...nextauth]/route.ts
@@ -1,41 +1,81 @@
 import NextAuth from 'next-auth';
-import CredentialsProvider  from 'next-auth/providers/credentials';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import { User } from '@/lib/definition';
 
+const mockDatabase: (User & { password: string })[] = [
+    {
+        id: '1',
+        email: 'test@test.com',
+        password: 'password123',
+        firstName: 'John',
+        lastName: 'Doe',
+        address: {
+            number: '123',
+            street: 'Main St',
+            complement: '',
+            postalCode: '12345'
+        }
+    },
+    {
+        id: '2',
+        email: 'jane@doe.com',
+        password: 'mypassword',
+        firstName: 'Jane',
+        lastName: 'Doe',
+        address: {
+            number: '456',
+            street: 'Second St',
+            complement: 'Apt 1',
+            postalCode: '67890'
+        }
+    }
+];
+
 const handler = NextAuth({
+
+    pages: {
+        signIn: '/',
+    },
     providers: [
         CredentialsProvider({
+            id: 'credentials',
             name: 'Credentials',
             credentials: {
                 email: { label: "Email", type: "email" },
-                password: {  label: "Password", type: "password" }
+                password: { label: "Password", type: "password" }
             },
-            async authorize(credentials, req): Promise<User | null> {
 
-                if (!credentials) return null
+            async authorize(credentials): Promise<User | null> {
+                console.log('ok');
+                if (!credentials) return null;
 
                 const { email, password } = credentials;
 
-                const user = { 
-                    id: '1148', 
-                    email, 
-                    firstName: 'Sam', 
-                    lastName: 'az', 
-                    address: { 
-                        number: '123', 
-                        street: 'Main ', 
-                        postalCode: '12345'
-                    }
-                };
+                const user = mockDatabase.find(user => user.email === email && user.password === password);
 
-                if (password === 'password') {
-                    return user
+                if (user) {
+                    const { password, ...userWithoutPassword } = user;
+                    console.log(userWithoutPassword);
+                    return userWithoutPassword;
                 } else {
-                    return null
+                    return null;
                 }
             }
         })
-    ]
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) token.user = user;
+            return token;
+        },
+        async session({ session, token }) {
+            session.user = token.user as any;
+            return session;
+        },
+    },
 });
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SessionWrapper from "@/ui/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <SessionWrapper>
+        <body className={inter.className}>{children}</body>
+      </SessionWrapper>
     </html>
   );
 }
